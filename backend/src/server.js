6badc93f445a72008783a8145f92550893529e16
vtorres96 <victorcdc96@gmail.com>
2019-08-13 22:25:33 -0300

    enabling the io listen

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const routes = require("./routes");
+
 require("dotenv/config");
 
 const app = express();
-
 const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user } = socket.handshake.query;
+
+  console.log(user, socket.id);
+
+  connectedUsers[user] = socket.id;
+});
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${
@@ -19,9 +31,16 @@ mongoose.connect(
   }
 );
 
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes"));
+app.use(routes);
 
 server.listen(3333, () => {
   console.log("Servidor rodando na porta: 3333");
