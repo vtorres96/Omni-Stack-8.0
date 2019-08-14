23af3d6d6a89a7f9ec3a4e7458d0d8e130db65f6
vtorres96 <victorcdc96@gmail.com>
2019-08-13 22:24:18 -0300

    enabling match in real time

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -12,6 +12,19 @@ module.exports = {
       return res.status(400).send({ error: "Developer not exists" });
     }
 
+    if (targetDev.likes.includes(loggedDev._id)) {
+      const loggedSocket = req.connectedUsers[user];
+      const targetSocket = req.connectedUsers[id];
+
+      if (loggedSocket) {
+        req.io.to(loggedSocket).emit("match", targetDev);
+      }
+
+      if (targetSocket) {
+        req.io.to(targetSocket).emit("match", loggedDev);
+      }
+    }
+
     loggedDev.likes.push(targetDev._id);
 
     await loggedDev.save();
